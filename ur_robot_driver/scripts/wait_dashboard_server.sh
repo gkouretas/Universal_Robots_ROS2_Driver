f3d538a7a2a2a7e5433d478343a3d40f01482e1d
gkouretas <thegreekgeorge07@gmail.com>
2024-11-17 23:45:47 -0800

    Modified control launch file to include simulation, added blocking event to wait for connection to be established. Note, this requires updates to the ursim shell script to work

--- ur_robot_driver/scripts/wait_dashboard_server.sh
+++ ur_robot_driver/scripts/wait_dashboard_server.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
 
-netcat -z 192.168.56.101 29999
+help()
+{
+  # Display Help
+  echo "Wait until dashboard server is accepting commands"
+  echo
+  echo "Syntax: `basename "$0"` [-a]"
+  echo "options:"
+  echo "    -a     IP address to reach the robot"
+  echo "    -h             Print this Help."
+  echo
+}
+
+IP_ADDRESS=192.168.56.101
+
+while getopts ":ha:" option; do
+  case $option in
+    h) # display Help
+      help
+      exit;;
+    a) # IP address
+      IP_ADDRESS=${OPTARG}
+      ;;
+    \?) # invalid option
+      echo "Error: Invalid option"
+      help
+      exit;;
+  esac
+done
+
+netcat -z $IP_ADDRESS 29999
 while [ $? -eq 1 ]
 do
     echo "Dashboard server not accepting connections..."
     sleep 3
-    netcat -z 192.168.56.101 29999
+    netcat -z $IP_ADDRESS 29999
 done
 echo "Dashboard server connections are possible."
 sleep 5
